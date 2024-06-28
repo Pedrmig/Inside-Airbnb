78f466ac26e3dd7a3577cd2dbea08104014f4dd5
Pedrmig <pedro.ferraz10@hotmail.com>
2024-06-28 10:36:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -129,14 +129,12 @@ if selected == "Información":
         # 4. Vulcanes
         
         st.markdown("""
-            <div class="container">
-        <h1 class='centered-title-pg1'>Volcanes</h1>
-            <p class='justified-text-pg1'>Hawaii es un archipiélago de islas volcánicas en el Océano Pacífico. Las islas son el resultado de la actividad volcánica que comenzó hace millones de años.</h1>
-            <p class='justified-text-pg1'>La isla de Hawaii es el volcán más grande y activo del mundo.</p>
-            <p class='justified-text-pg1'>Es un volcán en escudo, lo que significa que es un volcán grande y de forma redondeada.</p>
-            <p class='justified-text-pg1'>Las islas de Hawaii cuentan actualmente con 5 volcanes (Mauna Loa, Kohala, Hulalai, Kilaue y Mauna Kea) 3 de ellos activos. Mauna Loa no entraba en erupción desde 1984, pero tuvo una erupción sin víctimas mortales en 2022.</p>
-            </div>
-        """, unsafe_allow_html=True)
+            ### Volcanes              
+        """)
+        st.write('Hawaii es un archipiélago de islas volcánicas en el Océano Pacífico. Las islas son el resultado de la actividad volcánica que comenzó hace millones de años.)
+        st.write('La isla de Hawaii es el volcán más grande y activo del mundo.)
+        st.write('Es un volcán en escudo, lo que significa que es un volcán grande y de forma redondeada.</p>
+        st.write('Las islas de Hawaii cuentan actualmente con 5 volcanes (Mauna Loa, Kohala, Hulalai, Kilaue y Mauna Kea) 3 de ellos activos. Mauna Loa no entraba en erupción desde 1984, pero tuvo una erupción sin víctimas mortales en 2022.)
         st.image('https://static.temblor.net/wp-content/uploads/2018/05/hawaii-sp-7.jpg', use_column_width=True)
 
 
