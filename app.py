93844d3bfacf642e3b047fd8609f206f4b0c2b5d
Pedrmig <pedro.ferraz10@hotmail.com>
2024-06-28 11:07:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -133,21 +133,23 @@ if selected == "Precios":
         html_data = f.read()
     components.html(html_data, height=450)
 
-    st.markdown('Precio promedio por dia para 2 personas')    
-    with open("graficos/Average_Price_2_persons.html", "r", encoding='utf-8') as f:     
-        html_data = f.read()
-    components.html(html_data, height=450)
-        
     st.markdown('Precio promedio por vecindario')
     st.image('graficos/precio_medio_vecindario.png', use_column_width=True)
+    
+    st.markdown('Precio promedio por dia para 2 personas') 
+    with open("graficos/Average_Price_2_persons.html", "r", encoding='utf-8') as f:     
+        html_data = f.read()
+    components.html(html_data, height=450, width=950)
+        
     with open("graficos/precio_vecindario.html", "r", encoding='utf-8') as f:     
         html_data = f.read()
-    components.html(html_data, height=450)
+    components.html(html_data, height=450, width=950)
 
     st.markdown('Precio promedio por vecindario y tipo de habitación')
     with open("graficos/precio_habitacion_vecindario_box.html", "r", encoding='utf-8') as f:     
         html_data = f.read()
-    components.html(html_data, height=450)
+    components.html(html_data, height=450, width=950)
+    
     st.image('graficos/precioxhabitacion_swarmplot.png', use_column_width=True)
 
 # PAGE 4----------------------------------
